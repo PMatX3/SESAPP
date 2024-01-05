1dbff9b38db216bc718e00ad8a90a553c9279703
PMatX3 <patrick.mattis@googlemail.com>
2024-01-05 00:39:27 +0000

    added vector store and created conversation chain and resolved chat history

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationRetrievalChain
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+
 
 
 
@@ -32,47 +34,59 @@ def get_vector_store(text_chunks):
 
 def get_conversation_chain(vector_store):
     llm = ChatOpenAI()
-    conversation = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    conversation_chain = ConversationRetrievalChain.from_llm(llm=llm, retriever=vector_store.as.retriever, memory=memory)
-
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
     return conversation_chain
 
+def handle_userinput(user_question):
+    #st.write(user_template.replace("{{MSG}}", user_question), unsafe_allow_html=True)
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
 
-
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 def main():
     load_dotenv()
     #st.set_page_config(page_title="Find Your Candidate", page_icon="🇺🇸", layout="wide")
     st.set_page_config(page_title="Find Your Candidate", page_icon="Iraq", layout="wide")
+    st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+
     st.header("Find Your Candidate")
-    st.text_input("Ask a question about a candidate:")
+    user_question = st.text_input("Ask a question about a candidate:")
+    if user_question:
+        handle_userinput(user_question)
+
+    st.write(user_template.replace("{{MSG}}","Hello BestCandidate AI"), unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}", "Hello Human from SES."), unsafe_allow_html=True)
 
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader("Upload PDFs of your resume/CVs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
-            st.spinner("Processing...")
-            #get pdf text
-            raw_text = get_pdf_text(pdf_docs)
+            with st.spinner("Processing..."):
+                #get pdf text
+                raw_text = get_pdf_text(pdf_docs)
             
-
-            #get text chunks
-            text_chunks = get_text_chunks(raw_text)
+                #get text chunks
+                text_chunks = get_text_chunks(raw_text)
             
-            #create vector store   
-            vector_store = get_vector_store(text_chunks)
+                #create vector store   
+                vector_store = get_vector_store(text_chunks)
 
-
-            # create conversation chain
-            st.session_state.conversation = get_conversation_chain(vector_store)
-    
-    
-
-    
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vector_store)
+      
 
 if __name__ == "__main__":
     main()  # execute only if run as a script
--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://ibb.co/Q6s5QpT/SES-logo.png" alt="SES-logo" border="0" width="400" height="400">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
